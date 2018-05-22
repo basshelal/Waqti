91f28303a16244499f500ea913355673ca0e51f9
Bassam Helal <basshelal@gmail.com>
2018-05-22 08:46:04 +0100

    Test Commit

--- app/src/main/java/uk/whitecrescent/waqti/MainActivity.kt
+++ app/src/main/java/uk/whitecrescent/waqti/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
+        //some change
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
