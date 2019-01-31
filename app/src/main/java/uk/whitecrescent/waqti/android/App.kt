40a47d69a57b3763d0e0fc03b63dcc8c4ae05aa5
Bassam Helal <basshelal@gmail.com>
2019-01-31 23:59:13 +0000

    Updated Gradle dependencies again

--- app/src/main/java/uk/whitecrescent/waqti/android/App.kt
+++ app/src/main/java/uk/whitecrescent/waqti/android/App.kt
@@ -2,6 +2,7 @@ package uk.whitecrescent.waqti.android
 
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
+import com.squareup.leakcanary.LeakCanary
 import uk.whitecrescent.waqti.model.persistence.Caches
 import uk.whitecrescent.waqti.model.persistence.Database
 
@@ -9,6 +10,12 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
         AndroidThreeTen.init(this)
         Database.build(this)
         Caches.initialize()
